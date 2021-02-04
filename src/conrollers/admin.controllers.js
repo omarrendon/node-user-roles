b66428bd7e9859461470862599e1cb85ac8b6592
omar rendon <omarcr.96@gmail.com>
2021-02-04 13:52:17 -0600

    finish admin crud

--- src/conrollers/admin.controllers.js
+++ src/conrollers/admin.controllers.js
@@ -27,4 +27,163 @@ async function getAdministrators(req, res) {
   }
 }
 
-module.exports = { getAdministrators };
+async function getOneAdmin(req, res) {
+  const { admin_id } = req.params;
+
+  try {
+    const oneAdmin = await admin.findOne({
+      where: { admin_id },
+      attributes: [
+        "admin_id",
+        "nombre",
+        "apellido_paterno",
+        "apellido_materno",
+        "email",
+        "role",
+      ],
+    });
+
+    res.status(200).json({
+      ok: true,
+      message: "Administrador obtenido exitosamente",
+      data: oneAdmin,
+    });
+  } catch (err) {
+    res.status(500).json({
+      ok: false,
+      message: "No se encontraro ningún usuario",
+    });
+    console.log("ERROR POR BUSQUEDA DE ID" + err);
+  }
+}
+
+async function createAdmin(req, res) {
+  try {
+    const {
+      nombre,
+      apellido_paterno,
+      apellido_materno,
+      email,
+      contrasenia,
+      role,
+    } = req.body;
+
+    let newAdmin = await admin.create(
+      {
+        nombre,
+        apellido_paterno,
+        apellido_materno,
+        email,
+        contrasenia,
+        role,
+      },
+      {
+        fields: [
+          "nombre",
+          "apellido_paterno",
+          "apellido_materno",
+          "email",
+          "contrasenia",
+          "role",
+        ],
+      }
+    );
+
+    res.status(200).json({
+      ok: true,
+      message: "Administrador creado exitosamente",
+      data: newAdmin,
+    });
+  } catch (err) {
+    res.status(500).json({
+      ok: false,
+      message: "No se pudo crear un nuevo administrador",
+    });
+    console.log("Error :" + err);
+  }
+}
+
+async function updateAdmin(req, res) {
+  const { admin_id } = req.params;
+  const {
+    nombre,
+    apellido_paterno,
+    apellido_materno,
+    email,
+    contrasenia,
+    role,
+  } = req.body;
+
+  try {
+    const id = await admin.findAll({
+      attributes: [
+        "nombre",
+        "apellido_paterno",
+        "apellido_materno",
+        "email",
+        "contrasenia",
+        "role",
+      ],
+      where: {
+        admin_id,
+      },
+    });
+
+    const idUpdate = admin.update(
+      {
+        nombre,
+        apellido_paterno,
+        apellido_materno,
+        email,
+        contrasenia,
+        role,
+      },
+      {
+        where: { admin_id },
+      }
+    );
+
+    res.status(200).json({
+      ok: true,
+      message: "Administrador actualizado exitosamente",
+      data: id,
+    });
+  } catch (err) {
+    res.status(500).json({
+      ok: false,
+      message: "No se pudo actualizadar el administrador",
+    });
+    console.log("Error : " + err);
+  }
+}
+
+async function deleteAdmin(req, res) {
+  const { admin_id } = req.params;
+  try {
+    const elimnar = await admin.destroy({
+      where: {
+        admin_id,
+      },
+    });
+
+    res.status(200).json({
+      ok: true,
+      message: "Administrador eliminado exitosamente",
+      data: elimnar,
+    });
+  } catch (err) {
+    res.status(500).json({
+      ok: false,
+      message: "No se pudo eliminar el administrador",
+    });
+    console.log("ERROR :" + err);
+  }
+}
+
+module.exports = {
+  getAdministrators,
+  createAdmin,
+  getOneAdmin,
+  updateAdmin,
+  deleteAdmin,
+};
--- src/routes/admin.router.js
+++ src/routes/admin.router.js
@@ -1,8 +1,18 @@
 const { Router } = require("express");
 const router = Router();
 
-const { getAdministrators } = require("../conrollers/admin.controllers");
+const {
+  getAdministrators,
+  createAdmin,
+  getOneAdmin,
+  updateAdmin,
+  deleteAdmin,
+} = require("../conrollers/admin.controllers");
 
 router.get("/", getAdministrators);
+router.get("/:admin_id", getOneAdmin);
+router.post("/", createAdmin);
+router.put("/:admin_id", updateAdmin);
+router.delete("/:admin_id", deleteAdmin);
 
 module.exports = router;
